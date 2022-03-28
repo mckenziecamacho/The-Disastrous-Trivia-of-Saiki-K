70c06e787a92bda63285711a25f751b00cb2e880
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-03-28 12:32:08 -0400

    commit 2

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-let question = {
-    questionOne: 'What is Saiki\'s favorite food?',
+let firstQuestion = {
+    question: 'What is Saiki\'s favorite food?',
     answers: ['Ramen', 'Miso Soup', 'Coffee Jelly', 'Tonkatsu'],
+    correctAnswer: 2
+}
+
+let secondQuestion = {
+    question: 'Who is Saiki\'s best friend?',
+    answers: ['Teruhashi', 'Nendo', 'Kaido', 'No one. All of Saiki\'s friends annoy him'],
     correctAnswer: 3
+}
+
+let thirdQuestion = {
+    question: ['What is Saiki\'s first name?'],
+    answers: ['Kaoru', 'Katsuo', 'Kuriko', 'Kusuo'],
+    correctAnswer: 3
+}
+
+let fourthQuestion = {
+    question: ['Who is in love with Saiki?'],
+    answers: ['Teruhashi', 'Chiyo', 'Mera', 'Mikoto'],
+    correctAnswer: 0
+}
+
+let fifthQuestion = {
+    question: ['How does Teruhashi view herself?'],
+    answers: ['The cool girl', 'Perfect pretty girl', 'Normal girl', 'Most beautiful girl'],
+    correctAnswer: 1
+}
+
+let sixthQuestion = {
+    question: ['What is Saiki afraid of?'],
+    answers: ['Being alone', 'Cats', 'Heights', 'Bugs'],
+    correctAnswer: 3
+}
+
+let seventhQuestion = {
+    question: ['What evil organization does Kaido fight against?'],
+    answers: ['The Twelve Demon Moons', 'Dark Reunion', 'Akatsuki', 'The League of Villans'],
+    correctAnswer: 1
 }
