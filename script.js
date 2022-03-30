565937d45586dfeee52ee403269de83ee9a50f58
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-03-30 09:24:15 -0400

    commit 8

--- script.js
+++ script.js
@@ -1,44 +1,90 @@
-let firstQuestion = {
+let myQuestions = [
+{
     question: 'What is Saiki\'s favorite food?',
-    answers: ['Ramen', 'Miso Soup', 'Coffee Jelly', 'Tonkatsu'],
-    correctAnswer: 2
-} 
-
-let secondQuestion = {
+    answers: {
+        a: 'Ramen',
+        b: 'Miso Soup',
+        c: 'Coffee Jelly',
+        d: 'Tonkatsu'
+    },
+    correctAnswer: 'c'
+},
+{
     question: 'Who is Saiki\'s best friend?',
-    answers: ['Teruhashi', 'Nendo', 'Kaido', 'No one. All of Saiki\'s friends annoy him'],
-    correctAnswer: 3
+    answers: {
+        a: 'Terauhashi',
+        b: 'Nendo',
+        c: 'Kaido',
+        d: 'No one. All of Saiki\'s friends annoy him'
+    },
+    correctAnswer: 'd'
+},
+{
+    question: 'What is Saiki\'s first name?',
+    answers: {
+        a: 'Kaoru',
+        b: 'Katsuo',
+        c: 'Kuriko',
+        d: 'Kusuo'
+    },
+    correctAnswer: 'd'
+},
+{
+    question: 'Who is in love with Saiki?',
+    answers: {
+        a: 'Teruhashi',
+        b: 'Chiyo',
+        c: 'Mera',
+        d: 'Mikoto'
+    },
+    correctAnswer: 'a'
+},
+{
+    question: 'How does Teruhashi view herself?',
+    answers: {
+        a: 'The cool girl',
+        b: 'Perfect pretty girl',
+        c: 'Normal girl',
+        d: 'Most beautiful girl'
+    },
+    correctAnswer: 'b'
+},
+{
+    question: 'What is Saiki afraid of?',
+    answers: {
+        a: 'Being Alone',
+        b: 'Cats',
+        c: 'Heights',
+        d: 'Bugs'
+    },
+    correctAnswer: 'd'
+},
+{
+    question: 'What evil organization does Kaido fight against?',
+    answers: {
+        a: 'The Twelve Demon Moons',
+        b: 'Dark Reunion',
+        c: 'Akatsuki',
+        d: 'The League of Villans'
+    },
+    correctAnswer: 'b'
 }
+];
 
-let thirdQuestion = {
-    question: ['What is Saiki\'s first name?'],
-    answers: ['Kaoru', 'Katsuo', 'Kuriko', 'Kusuo'],
-    correctAnswer: 3
-}
 
-let fourthQuestion = {
-    question: ['Who is in love with Saiki?'],
-    answers: ['Teruhashi', 'Chiyo', 'Mera', 'Mikoto'],
-    correctAnswer: 0
-}
 
-let fifthQuestion = {
-    question: ['How does Teruhashi view herself?'],
-    answers: ['The cool girl', 'Perfect pretty girl', 'Normal girl', 'Most beautiful girl'],
-    correctAnswer: 1
-}
 
-let sixthQuestion = {
-    question: ['What is Saiki afraid of?'],
-    answers: ['Being alone', 'Cats', 'Heights', 'Bugs'],
-    correctAnswer: 3
-}
 
-let seventhQuestion = {
-    question: ['What evil organization does Kaido fight against?'],
-    answers: ['The Twelve Demon Moons', 'Dark Reunion', 'Akatsuki', 'The League of Villans'],
-    correctAnswer: 1
-}
+
+
+
+
+
+
+
+
+
+
 
 let questions = document.querySelector('#question');
 let answers = document.getElementById('answer-buttons');
